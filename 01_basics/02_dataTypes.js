bdb0863fb38e775f754b1e59d9643d99d6a2a30f
shivam_hyperlink <shivamp@hyperlinkinfosystem.net.in>
2024-03-28 23:38:33 +0530

    feat: basic data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // It doesn't works in node, it works in browser
+
+let name = "Shivam"
+let age = 21
+let isLoggedIn = true
+let undefinedVariable
+
+// Primitive
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/falae
+null => standalone value
+undefined => 
+symbol => used for unique
+*/
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
