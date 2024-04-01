f23605dc1aa3e566374df8135bc465b48800a3d0
shivam_hyperlink <shivamp@hyperlinkinfosystem.net.in>
2024-04-01 23:38:41 +0530

    feat: data operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -53,4 +53,29 @@ let str1 = "Shivam ";
 let str2 = "Patel";
 
 let str3 = str1 + str2;
-console.log(str3);
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let x2 = 3;
+const y2 = x2++;
+
+console.log(`x:${x2}, y:${y2}`);
+
+let preGameCounter = 100;
+++preGameCounter;
+// console.log(preGameCounter);
+
+// link for ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
